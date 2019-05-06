664083dfef6c37513e12a1f8ac79ba0510090492
renyb <renyb@workai.com.cn>
2019-05-06 16:23:30 +0800

    本地练习项目迁移

---
+++ client/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// const handleTest = (x, y) => {
+//     return x + y;
+// }
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>this is a 11test!</div>
+        )
+    }
+}
---
+++ client/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import les from './app.less';
+
+ReactDOM.render(<App />,document.body)
+
+// alert('333');
